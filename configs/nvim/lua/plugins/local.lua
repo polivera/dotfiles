102c42677e7dfd285692b62af7147526e5946cd1
polivera <punk.moon2229@fastmail.com>
2024-01-27 20:38:14 +0100

    Add custom plugin to run commands on file save

--- configs/nvim/lua/plugins/local.lua
+++ configs/nvim/lua/plugins/local.lua
@@ -5,7 +5,11 @@ print(vim.fn.stdpath("config").."/lua/custom/ortiexec.nvim")
 return {
   dir = vim.fn.stdpath("config") .. "/lua/custom/ortiexec.nvim",
   config = function()
-    require("ortiexec").setup()
+    require("ortiexec").setup({
+      ["*.lua"] = {
+        ""
+      }
+    })
   end
-
 }
+
