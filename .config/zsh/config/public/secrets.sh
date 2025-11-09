4fc1ef32a5484a10c9265e4b3f6e9af4ae66c0dc
polivera <punk.moon2229@fastmail.com>
2025-11-09 14:03:52 +0100

    Adding command check to secrets.sh

--- .config/zsh/config/public/secrets.sh
+++ .config/zsh/config/public/secrets.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Load encrypted environment variables using sops
-if command -v sops &> /dev/null; then
+if command -v sops &> /dev/null && command -v yq &> /dev/null; then
     if [ -f "$HOME/.config/secrets/env.yaml" ]; then
         eval "$(sops -d "$HOME/.config/secrets/env.yaml" | yq eval 'to_entries | .[] | "export " + .key + "=\"" + .value + "\""' -)"
     fi
+else
+    echo "sops and/or yq are not installed"
 fi
