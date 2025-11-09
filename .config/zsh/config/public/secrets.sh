ea7f7addfdc12f68c250f51fde8330571d806e74
polivera <punk.moon2229@fastmail.com>
2025-11-09 21:49:54 +0100

    Fixing secrets.sh on linux

--- .config/zsh/config/public/secrets.sh
+++ .config/zsh/config/public/secrets.sh
@@ -3,7 +3,14 @@
 # Load encrypted environment variables using sops
 if command -v sops &> /dev/null && command -v yq &> /dev/null; then
     if [ -f "$HOME/.config/secrets/env.yaml" ]; then
-        eval "$(sops -d "$HOME/.config/secrets/env.yaml" | yq eval 'to_entries | .[] | "export " + .key + "=\"" + .value + "\""' -)"
+        # Detect yq version (mikefarah's yq vs Python yq)
+        if [[ "$XAP_OS" == "macos" ]]; then
+            # mikefarah's yq (Go version) - used on Mac
+            eval "$(sops -d "$HOME/.config/secrets/env.yaml" | yq eval 'to_entries | .[] | "export " + .key + "=\"" + .value + "\""' -)"
+        else
+            # Python yq (jq wrapper) - common on Linux
+            eval $(sops -d "$HOME/.config/secrets/env.yaml" | yq -r 'to_entries | .[] | "export \(.key)=\"\(.value)\""')
+        fi
     fi
 else
     echo "sops and/or yq are not installed"
